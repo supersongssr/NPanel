6b33dff10e5c0d6ecf6677650091ba33d2e33740
supersongssr <34210429+supersongssr@users.noreply.github.com>
2019-01-19 10:41:04 +0800

    change 300

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -328,7 +328,7 @@ class AuthController extends Controller
 
                     User::query()->where('id', $referral_uid)->increment('transfer_enable', $transfer_enable);
                     //song add money
-                    User::query()->where('id', $referral_uid)->increment('balance', '100');
+                    User::query()->where('id', $referral_uid)->increment('balance', '300');
                     //
                     User::query()->where('id', $referral_uid)->update(['status' => 1, 'enable' => 1]);
                 }
