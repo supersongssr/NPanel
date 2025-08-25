2984d030d4a019e4a943b49abb66396c33f09615
supersongssr <mumaxiaoyaostore@outlook.com>
2025-08-25 14:18:53 +0800

    修复节点过期时间查询格式

--- app/Http/Controllers/Api/PingController.php
+++ app/Http/Controllers/Api/PingController.php
@@ -122,7 +122,7 @@ class PingController extends Controller
 
         // 获取一个可用的 nodeid
         if (!empty($request->get('new_node_id'))) {
-            $node = SsNode::query()->where('id','>',99)->where('heartbeat_at','<', date(time() - 604800))->where('status', 0)->orderBy('id', 'asc')->first();
+            $node = SsNode::query()->where('id','>',99)->where('heartbeat_at','<', date('Y-m-d H:i:s',time() - 604800))->where('status', 0)->orderBy('id', 'asc')->first();
             if ($node ) { // 过期7天
                 $node->heartbeat_at = date('Y-m-d H:i:s');
                 $node->save();
