f8e98abb07d0721ad7cc9a254c3bc541bc09cd11
root <root@kr>
2026-01-13 10:12:29 +0800

    fix: singbox 一些弃用警告,已兼容 singbox v1.11 12 13

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -848,7 +848,9 @@ class SubscribeController extends Controller
                     'type' => 'tun',
                     'tag' => 'tun-in',
                     'interface_name' => 'tun0',
-                    'inet4_address' => '172.19.0.1/30',
+                    'address' => [
+                        '172.19.0.1/30'
+                    ],
                     'auto_route' => true,
                     'strict_route' => true,
                     'sniff' => true,
